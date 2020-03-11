2e497af5342ef83a716c4c37b7c02ab07d095bd5
Liangying Wei <lianwei@microsoft.com>
2020-03-11 16:38:24 +0800

    Add simple chat demo

--- serverless-websocket/samples/simple-chat/Readme.md
+++ serverless-websocket/samples/simple-chat/Readme.md
@@ -1,2 +1,50 @@
 # A Simple Chat on WebSocket
-Adding....
+This is a simple chat demo. The client side uses the `WebSocket` Web API and is connected to Azure SignalR Serverless WebSocket acting as a reverse proxy. There is no need for the server side to be a WebSocket server, the server side, hosted in [node.js](), only need to handle bussiness logic.
+
+## Run the demo locally
+### Prerequisites
+* [Node.js]() to host your server locally
+* [ngrok](https://ngrok.com/) to expose local port to public
+
+### Steps
+1. Update `AzureSignalRConnectionString` in [settings.json](./settings.json) with your connection string.
+2. Under this folder, run the local server by:
+```
+npm install
+node index.js
+```
+The local server now listens the `8088` port.
+
+3. Expose the local port using [ngrok](https://ngrok.com/)
+Go to the path where `ngrok` exists, type:
+```
+ngrok http 8088
+```
+Now local server has a public host name `xxxxx.ngrok.io`. As in the below screenshot, the public host name is `http://f0d3237b.ngrok.io`.
+
+![ngrok](./images/sample_ngrok.png)
+
+4. Set the Upstream pattern for your service
+We have the [Upstream Manage Page](https://ws-manage.azurewebsites.net/api/manage) as a **temp** workaround before the Upstream settings are available in the Azure portal.
+
+Open the [page](https://ws-manage.azurewebsites.net/api/manage) and input your `ConnectionString`, set the upstream for hub `simplechat` to the textarea for **Set Upstream Settings**, remember to replace `xxxxx` with your `ngrok` host. Our sample server listens to path `/simplechat/connect`, `/simplechat/disconnect` and `/simplechat/message`, from which `simplechat` is the value of the `hub`, and `connect`, `disconnect` and `message` are the name of the events. For details about the terms, please refer to the [spec](../../specs/runtime-websocket-serverless.md).
+
+```json
+{
+	"templates": [
+		{
+			"hubPattern": "simplechat",
+			"urlTemplate": "http://xxxxx.ngrok.io/{hub}/{event}"
+		}
+	]
+}
+```
+
+For example, the below screenshot sets the upstream templates for both `chat` and `simplechat` hubs.
+![ngrok](./images/sample_set_upstream.png)
+
+5. Visit the chat
+Now it is all set, visit the client page `http://localhost:8088?name={user_name}` with your `{user_name}` to try the Chat.
+![chat](./images/sample_run.png)
+
+>>>>>>> fdb9a47... Add simple chat demo
---
+++ serverless-websocket/samples/simple-chat/index.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const axios = require("axios");
+const config = require('./settings.json');
+
+const app = express();
+const conn = config["AzureSignalRConnectionString"];
+if (!conn){
+  console.error("Please specify `AzureSignalRConnectionString` with your connection string in settings.json file!");
+  return;
+}
+const parsed = parse(conn);
+if (!parsed) {
+  console.error(`Set valid connectiong string in settins.json: ${conn}`);
+  return;
+}
+
+const hub = 'simplechat';
+
+// serve default to return index.html
+app.get('/', function (req, res) {
+  const name = req.query.name;
+  if (!name) {
+    res.status(404).send("User name is not found in query 'name'");
+    return;
+  }
+
+  fs.readFile(path.join(__dirname, 'index.html'), { encoding: 'utf8' }, function (err, data) {
+    if (err) {
+      res.sendStatus(404);
+    } else {
+      var rendered = data.replace(/%%%____URL____%%%/g, `${parsed.wshost}ws/client/hubs/${hub}/?user=${name}`)
+        .replace(/%%%___user___%%%/g, name);
+      res.send(rendered);
+    }
+  });
+});
+
+// serve requests for "simplechat"
+app.post(`/${hub}/connect`, function (req, res) {
+  var user = new URLSearchParams(req.header("x-asrs-client-query")).get('user');
+  console.log(`connect ${req.header("x-asrs-connection-id")} user ${user} connected.`);
+  // set back the user id for the connection
+  res.header("x-asrs-user-id", user);
+  res.send(`User ${user}(${req.header("x-asrs-connection-id")}) connected.`);
+});
+app.post(`/${hub}/message`, function (req, res) {
+  console.log(`User ${res.header("x-asrs-user-id")}(${req.header("x-asrs-connection-id")}) sending message.`);
+  // broadcast through Azure SignalR
+  req.setEncoding('utf8');
+  var body = '';
+  req.on('data', function(chunk) {
+    body += chunk;
+  });
+  
+  req.on('end', async function(){
+    if (body){
+      console.log("broadcasting " + body);
+      await broadcast(body);
+    }
+  });
+});
+app.post(`/${hub}/disconnect`, function (req, res) {
+  console.log(`User ${res.header("x-asrs-user-id")}(${req.header("x-asrs-connection-id")}) disconnected.`);
+});
+
+var server = app.listen(8088, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Serving http://%s:%s", host, port);
+});
+
+function broadcast(content) {
+  var path = "ws/api/v1/hubs/" + hub;
+  var url = parsed.host + path;
+  var token = getToken(path);
+  return axios.post(url, content, {
+    headers: {
+        "Content-Type": "text/plain",
+        "Authorization": token
+    }
+  });
+}
+
+function getToken(path) {
+  return "Bearer " + jwt.sign({}, parsed.key, {
+    audience: parsed.audience + path,
+    expiresIn: "1h",
+    algorithm: "HS256"
+  });
+}
+
+function parse(conn) {
+  const em = /Endpoint=(.*?);/g.exec(conn);
+  if (!em) return null;
+  const endpoint = em[1];
+  const km = /AccessKey=(.*?);/g.exec(conn);
+  if (!km) return null;
+  const key = km[1];
+  if (!endpoint || !key) return null;
+  const pm = /Port=(.*?);/g.exec(conn);
+  const port = pm == null ? '' : pm[1];
+  var url = new URL(endpoint);
+  url.port = port;
+  const host = url.toString();
+  url.port = '';
+  const audience = url.toString();
+  return {
+    host: host,
+    audience: audience,
+    key: key,
+    wshost: host.replace('https://', 'wss://').replace('http://', 'ws://')
+  };
+}
