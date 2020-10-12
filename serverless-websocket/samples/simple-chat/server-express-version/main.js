d092f1deed6e166ef6ecf432d10059efc1c709b2
Terence Fan <stdrickforce@gmail.com>
2020-10-12 17:04:20 +0800

    add req.rawBody (#12)

--- serverless-websocket/samples/simple-chat/server-express-version/main.js
+++ serverless-websocket/samples/simple-chat/server-express-version/main.js
@@ -4,39 +4,48 @@ const express = require('express');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    var data = "";
+    req.on('data', function(chunk) { data += chunk })
+    req.on('end', function() {
+        req.rawBody = data;
+        next();
+    })
+})
+
 // serve requests for "simplechat"
-app.post(`/simplechat/connect`, function (req, res) {
-  // simple auth to read user info from query
-  var event = client.event(req.headers);
-  var user = event.query.user;
-  console.log(`User ${user}(${event.connectionId}) connected, auth the user.`);
-  // set back the user id for the connection
-  res.header("x-asrs-user-id", user);
-  res.send('');
-});
-
-app.post(`/simplechat/message`, async function (req, res) {
-  var event = client.event(req.headers);
-  console.log(`User ${event.userId}(${event.connectionId}) sending message.`);
-  // broadcast through Azure SignalR
-  try {
-    await client.broadcast(req.body);
+app.post(`/simplechat/connect`, function(req, res) {
+    // simple auth to read user info from query
+    var event = client.event(req.headers);
+    var user = event.query.user;
+    console.log(`User ${user}(${event.connectionId}) connected, auth the user.`);
+    // set back the user id for the connection
+    res.header("x-asrs-user-id", user);
     res.send('');
-  } catch (err) {
-    // report err back to client
-    res.status(520).send({ type: 'error', content: err.message });
-    console.err(`Error broadcasting: ${err.message}`);
-  }
 });
 
-app.post(`/simplechat/disconnect`, function (req, res) {
-  var event = client.event(req.headers);
-  console.log(`User ${event.userId}(${event.connectionId}) disconnected.`);
-  res.send('');
+app.post(`/simplechat/message`, async function(req, res) {
+    var event = client.event(req.headers);
+    console.log(`User ${event.userId}(${event.connectionId}) sending message.`);
+    // broadcast through Azure SignalR
+    try {
+        await client.broadcast(req.rawBody);
+        res.send('');
+    } catch (err) {
+        // report err back to client
+        res.status(520).send({ type: 'error', content: err.message });
+        console.err(`Error broadcasting: ${err.message}`);
+    }
 });
 
-var server = app.listen(8088, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Serving http://%s:%s", host, port);
+app.post(`/simplechat/disconnect`, function(req, res) {
+    var event = client.event(req.headers);
+    console.log(`User ${event.userId}(${event.connectionId}) disconnected.`);
+    res.send('');
+});
+
+var server = app.listen(8088, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Serving http://%s:%s", host, port);
 });
